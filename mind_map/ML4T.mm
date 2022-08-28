e3030debdc22bc1e121f2f5cdf8471b039cb69c2
kyaiooiayk <mura.gabriele.luigi@gmail.com>
2022-08-28 13:57:09 +0100

    __

---
+++ mind_map/ML4T.mm
@@ -0,0 +1,65 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1661689069042" ID="ID_1500069584" MODIFIED="1661689074326" TEXT="ML4T">
+<node CREATED="1661689913287" ID="ID_364931973" MODIFIED="1661689924380" POSITION="left" TEXT="Quantitative strategies history">
+<icon BUILTIN="xmag"/>
+<node CREATED="1661689926919" ID="ID_1098598077" MODIFIED="1661689996879" TEXT="1. Academic reasearch - 1980s-1990s">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1661689938124" ID="ID_1197998487" MODIFIED="1661689996879" TEXT="2. Factor-based investing - 2000s">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1661689977172" ID="ID_497689229" MODIFIED="1661689996879" TEXT="3. ML capabilities and aternatives">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1661690104223" ID="ID_363285369" MODIFIED="1661690132365" POSITION="right" TEXT="Data">
+<icon BUILTIN="xmag"/>
+<node CREATED="1661690110549" ID="ID_1717789771" MODIFIED="1661690128517" TEXT="Fundamental Data">
+<icon BUILTIN="wizard"/>
+</node>
+<node CREATED="1661689127295" ID="ID_1040980611" MODIFIED="1661689334639" TEXT="Alternative Data">
+<icon BUILTIN="xmag"/>
+<node CREATED="1661689281944" ID="ID_1804773283" MODIFIED="1661689303452" TEXT="What?">
+<icon BUILTIN="xmag"/>
+<node CREATED="1661689140219" ID="ID_1827614433" MODIFIED="1661689308924" TEXT="Online price data">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1661689226368" ID="ID_1635318793" MODIFIED="1661689308924" TEXT="Store visits &amp; purchaises">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1661689236829" ID="ID_1195845649" MODIFIED="1661689308923" TEXT="Satellite images">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1661689419601" ID="ID_1869643221" MODIFIED="1661689442513" TEXT="Sources">
+<icon BUILTIN="xmag"/>
+<node CREATED="1661689423732" ID="ID_1685551765" MODIFIED="1661689438117" TEXT="Individuals">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1661689428734" ID="ID_483589062" MODIFIED="1661689438118" TEXT="Businesses">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1661689431165" ID="ID_1734142419" MODIFIED="1661689438118" TEXT="Sensors">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1661689288063" ID="ID_623851310" MODIFIED="1661689417966" TEXT="Metrics">
+<icon BUILTIN="xmag"/>
+<node CREATED="1661689312830" ID="ID_1631340761" MODIFIED="1661689329747" TEXT="Momentum">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1661689316344" ID="ID_74389022" MODIFIED="1661689329748" TEXT="Value">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1661689318372" ID="ID_861583398" MODIFIED="1661689329748" TEXT="Quality">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1661689321407" ID="ID_1041987057" MODIFIED="1661689329748" TEXT="Sentiment">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>

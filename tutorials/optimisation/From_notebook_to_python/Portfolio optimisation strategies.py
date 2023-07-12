6db571849101a3e24384bd45f6d9756329c05b9a
kyaiooiayk <mura.gabriele.luigi@gmail.com>
2023-07-12 10:25:07 +0100

    _

--- tutorials/optimisation/From_notebook_to_python/Portfolio optimisation strategies.py
+++ tutorials/optimisation/From_notebook_to_python/Portfolio optimisation strategies.py
@@ -9,9 +9,9 @@
 
 # <div class="alert alert-warning">
 # <font color=black>
-# 
+#
 # **What?** Mean-Variance optimisation
-# 
+#
 # </font>
 # </div>
 
@@ -20,22 +20,22 @@
 
 # <div class="alert alert-info">
 # <font color=black>
-# 
-# - MPT solves for the optimal portfolio weights to minimize volatility for a given expected return, or maximize returns for a given level of volatility. The key requisite input are expected asset returns, standard deviations, and the covariance matrix. 
-# 
+#
+# - MPT solves for the optimal portfolio weights to minimize volatility for a given expected return, or maximize returns for a given level of volatility. The key requisite input are expected asset returns, standard deviations, and the covariance matrix.
+#
 # - Diversification works because the variance of portfolio returns depends on the covariance of the assets and can be reduced below the weighted average of the asset variances by including assets with less than perfect correlation. In particular, given a vector, ω, of portfolio weights and the covariance matrix, $\Sigma$, the portfolio variance, $\sigma_{\text{PF}}$ is defined as:
 # $$\sigma_{\text{PF}}=\omega^T\Sigma\omega$$
-# 
+#
 # - Markowitz showed that the problem of maximizing the expected portfolio return subject to a target risk has an equivalent dual representation of minimizing portfolio risk subject to a target expected return level, $μ_{PF}$. Hence, the optimization problem becomes:
 # $$
 # \begin{align}
 # \min_\omega & \quad\quad\sigma^2_{\text{PF}}= \omega^T\Sigma\omega\\
-# \text{s.t.} &\quad\quad \mu_{\text{PF}}= \omega^T\mu\\ 
+# \text{s.t.} &\quad\quad \mu_{\text{PF}}= \omega^T\mu\\
 # &\quad\quad \lVert\omega\rVert =1
 # \end{align}
 # $$
-# 
-# 
+#
+#
 # </font>
 # </div>
 
@@ -44,11 +44,11 @@
 
 # <div class="alert alert-info">
 # <font color=black>
-# 
-# - The covariance matrix can be estimated somewhat more reliably, which has given rise to several alternative approaches. However, covariance matrices with correlated assets pose computational challenges since the optimization problem requires inverting the matrix. 
-# 
+#
+# - The covariance matrix can be estimated somewhat more reliably, which has given rise to several alternative approaches. However, covariance matrices with correlated assets pose computational challenges since the optimization problem requires inverting the matrix.
+#
 # - The high condition number induces numerical instability, which in turn gives rise to the **Markovitz curse**: the more diversification is required (by correlated investment opportunities), the more unreliable the weights produced by the algorithm.
-# 
+#
 # </font>
 # </div>
 
@@ -76,7 +76,7 @@ from pathlib import Path
 # In[2]:
 
 
-sns.set_style('whitegrid')
+sns.set_style("whitegrid")
 np.random.seed(42)
 
 
@@ -91,16 +91,16 @@ cmap = sns.diverging_palette(10, 240, n=9, as_cmap=True)
 
 # <div class="alert alert-info">
 # <font color=black>
-# 
-# - These are the instructions for downloading the Quandl Wiki stock prices. 
+#
+# - These are the instructions for downloading the Quandl Wiki stock prices.
 # - Step-by-step procedure:
 #     1. Follow the instructions to create a free [NASDAQ account](https://data.nasdaq.com/sign-up)
-#     2. If the link above does not work use [this]((https://data.nasdaq.com/databases/WIKIP/documentation)) one to sign-up 
+#     2. If the link above does not work use [this]((https://data.nasdaq.com/databases/WIKIP/documentation)) one to sign-up
 #     3. [Download](https://data.nasdaq.com/tables/WIKIP/WIKI-PRICES/export) the entire WIKI/PRICES data
 #     4. Extract the .zip file,
 #     5. Move to this directory and rename to `wiki_prices.csv`
-#     6. Run the below code to store in fast HDF format. 
-# 
+#     6. Run the below code to store in fast HDF format.
+#
 # </font>
 # </div>
 
@@ -108,18 +108,19 @@ cmap = sns.diverging_palette(10, 240, n=9, as_cmap=True)
 
 
 # Check if .csv is present
-get_ipython().system('ls *.csv')
+get_ipython().system("ls *.csv")
 
 
 # In[5]:
 
 
 # read csv with pandas
-df = (pd.read_csv('wiki_prices.csv',
-                 parse_dates=['date'],
-                 index_col=['date', 'ticker'],
-                 infer_datetime_format=True)
-     .sort_index())
+df = pd.read_csv(
+    "wiki_prices.csv",
+    parse_dates=["date"],
+    index_col=["date", "ticker"],
+    infer_datetime_format=True,
+).sort_index()
 
 
 # In[6]:
@@ -133,9 +134,9 @@ df.info(null_counts=True)
 
 
 # Put data into an h5 format
-DATA_STORE = Path('assets.h5')
+DATA_STORE = Path("assets.h5")
 with pd.HDFStore(DATA_STORE) as store:
-    store.put('quandl/wiki/prices', df)
+    store.put("quandl/wiki/prices", df)
 
 
 # - The following code downloads the current S&P 500 constituents from [Wikipedia](https://en.wikipedia.org/wiki/List_of_S%26P_500_companies).
@@ -143,7 +144,7 @@ with pd.HDFStore(DATA_STORE) as store:
 # In[8]:
 
 
-url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
 df = pd.read_html(url, header=0)[0]
 
 
@@ -156,9 +157,18 @@ df.head(5)
 # In[10]:
 
 
-df.columns = ['ticker', 'name', 'sec_filings', 'gics_sector', 'gics_sub_industry',
-              'location', 'first_added', 'cik', 'founded']
-df = df.drop('sec_filings', axis=1).set_index('ticker')
+df.columns = [
+    "ticker",
+    "name",
+    "sec_filings",
+    "gics_sector",
+    "gics_sub_industry",
+    "location",
+    "first_added",
+    "cik",
+    "founded",
+]
+df = df.drop("sec_filings", axis=1).set_index("ticker")
 
 
 # In[11]:
@@ -172,15 +182,15 @@ print(df.info())
 
 # Saving data locally
 with pd.HDFStore(DATA_STORE) as store:
-    store.put('sp500/stocks', df)
+    store.put("sp500/stocks", df)
 
 
 # In[13]:
 
 
-# Reading the data from file 
-with pd.HDFStore('assets.h5') as store:
-    sp500_stocks = store['sp500/stocks']
+# Reading the data from file
+with pd.HDFStore("assets.h5") as store:
+    sp500_stocks = store["sp500/stocks"]
 
 
 # In[14]:
@@ -192,12 +202,13 @@ sp500_stocks.head()
 # In[15]:
 
 
-with pd.HDFStore('assets.h5') as store:
-    prices = (store['quandl/wiki/prices']
-              .adj_close
-              .unstack('ticker')
-              .filter(sp500_stocks.index)
-              .sample(n=30, axis=1))
+with pd.HDFStore("assets.h5") as store:
+    prices = (
+        store["quandl/wiki/prices"]
+        .adj_close.unstack("ticker")
+        .filter(sp500_stocks.index)
+        .sample(n=30, axis=1)
+    )
 
 
 # In[16]:
@@ -223,7 +234,9 @@ end = 2017
 # In[18]:
 
 
-weekly_returns = prices.loc[f'{start}':f'{end}'].resample('W').last().pct_change().dropna(how='all')
+weekly_returns = (
+    prices.loc[f"{start}":f"{end}"].resample("W").last().pct_change().dropna(how="all")
+)
 weekly_returns = weekly_returns.dropna(axis=1)
 weekly_returns.info()
 
@@ -246,7 +259,7 @@ n_assets, n_obs
 # In[21]:
 
 
-NUM_PF = 100000 # no of portfolios to simulate
+NUM_PF = 100000  # no of portfolios to simulate
 
 
 # In[22]:
@@ -261,7 +274,7 @@ x0 /= np.sum(np.abs(x0))
 # In[23]:
 
 
-periods_per_year = round(weekly_returns.resample('A').size().mean())
+periods_per_year = round(weekly_returns.resample("A").size().mean())
 periods_per_year
 
 
@@ -289,12 +302,14 @@ precision_matrix = pd.DataFrame(inv(cov_matrix), index=stocks, columns=stocks)
 # In[26]:
 
 
-treasury_10yr_monthly = (web.DataReader('DGS10', 'fred', start, end)
-                         .resample('M')
-                         .last()
-                         .div(periods_per_year)
-                         .div(100)
-                         .squeeze())
+treasury_10yr_monthly = (
+    web.DataReader("DGS10", "fred", start, end)
+    .resample("M")
+    .last()
+    .div(periods_per_year)
+    .div(100)
+    .squeeze()
+)
 
 
 # In[27]:
@@ -312,19 +327,26 @@ rf_rate = treasury_10yr_monthly.mean()
 
 
 def simulate_portfolios(mean_ret, cov, rf_rate=rf_rate, short=True):
-    alpha = np.full(shape=n_assets, fill_value=.05)
+    alpha = np.full(shape=n_assets, fill_value=0.05)
     weights = dirichlet(alpha=alpha, size=NUM_PF)
     if short:
         weights *= choice([-1, 1], size=weights.shape)
 
     returns = weights @ mean_ret.values + 1
-    returns = returns ** periods_per_year - 1
+    returns = returns**periods_per_year - 1
     std = (weights @ weekly_returns.T).std(1)
     std *= np.sqrt(periods_per_year)
     sharpe = (returns - rf_rate) / std
-    return pd.DataFrame({'Annualized Standard Deviation': std,
-                         'Annualized Returns': returns,
-                         'Sharpe Ratio': sharpe}), weights
+    return (
+        pd.DataFrame(
+            {
+                "Annualized Standard Deviation": std,
+                "Annualized Returns": returns,
+                "Sharpe Ratio": sharpe,
+            }
+        ),
+        weights,
+    )
 
 
 # In[29]:
@@ -344,26 +366,33 @@ df.describe()
 
 
 # Plot simulated portfolios
-ax = simul_perf.plot.scatter(x=0, y=1, c=2, cmap='Blues',
-                             alpha=0.5, figsize=(14, 9), colorbar=True,
-                             title=f'{NUM_PF:,d} Simulated Portfolios')
+ax = simul_perf.plot.scatter(
+    x=0,
+    y=1,
+    c=2,
+    cmap="Blues",
+    alpha=0.5,
+    figsize=(14, 9),
+    colorbar=True,
+    title=f"{NUM_PF:,d} Simulated Portfolios",
+)
 
 max_sharpe_idx = simul_perf.iloc[:, 2].idxmax()
 sd, r = simul_perf.iloc[max_sharpe_idx, :2].values
-print(f'Max Sharpe: {sd:.2%}, {r:.2%}')
-ax.scatter(sd, r, marker='*', color='darkblue', s=500, label='Max. Sharpe Ratio')
+print(f"Max Sharpe: {sd:.2%}, {r:.2%}")
+ax.scatter(sd, r, marker="*", color="darkblue", s=500, label="Max. Sharpe Ratio")
 
 min_vol_idx = simul_perf.iloc[:, 0].idxmin()
 sd, r = simul_perf.iloc[min_vol_idx, :2].values
-ax.scatter(sd, r, marker='*', color='green', s=500, label='Min Volatility')
-plt.legend(labelspacing=1, loc='upper left')
+ax.scatter(sd, r, marker="*", color="green", s=500, label="Min Volatility")
+plt.legend(labelspacing=1, loc="upper left")
 plt.tight_layout()
 
 
 # ## Compute Annualize PF Performance
 
-# Now we'll set up the quadratic optimization problem to solve for the minimum standard deviation for a given return or the maximum SR. 
-# 
+# Now we'll set up the quadratic optimization problem to solve for the minimum standard deviation for a given return or the maximum SR.
+#
 # To this end, define the functions that measure the key metrics:
 
 # In[32]:
@@ -407,21 +436,22 @@ def neg_sharpe_ratio(weights, mean_ret, cov):
 # In[36]:
 
 
-weight_constraint = {'type': 'eq', 
-                     'fun': lambda x: np.sum(np.abs(x))-1}
+weight_constraint = {"type": "eq", "fun": lambda x: np.sum(np.abs(x)) - 1}
 
 
 # In[37]:
 
 
 def max_sharpe_ratio(mean_ret, cov, short=False):
-    return minimize(fun=neg_sharpe_ratio,
-                    x0=x0,
-                    args=(mean_ret, cov),
-                    method='SLSQP',
-                    bounds=((-1 if short else 0, 1),) * n_assets,
-                    constraints=weight_constraint,
-                    options={'tol':1e-10, 'maxiter':1e4})
+    return minimize(
+        fun=neg_sharpe_ratio,
+        x0=x0,
+        args=(mean_ret, cov),
+        method="SLSQP",
+        bounds=((-1 if short else 0, 1),) * n_assets,
+        constraints=weight_constraint,
+        options={"tol": 1e-10, "maxiter": 1e4},
+    )
 
 
 # ## Compute Efficient Frontier
@@ -432,27 +462,26 @@ def max_sharpe_ratio(mean_ret, cov, short=False):
 
 
 def min_vol_target(mean_ret, cov, target, short=False):
-
     def ret_(wt):
         return portfolio_returns(wt, mean_ret)
 
-    constraints = [{'type': 'eq',
-                    'fun': lambda x: ret_(x) - target},
-                   weight_constraint]
+    constraints = [{"type": "eq", "fun": lambda x: ret_(x) - target}, weight_constraint]
 
     bounds = ((-1 if short else 0, 1),) * n_assets
-    return minimize(portfolio_std,
-                    x0=x0,
-                    args=(mean_ret, cov),
-                    method='SLSQP',
-                    bounds=bounds,
-                    constraints=constraints,
-                    options={'tol': 1e-10, 'maxiter': 1e4})
+    return minimize(
+        portfolio_std,
+        x0=x0,
+        args=(mean_ret, cov),
+        method="SLSQP",
+        bounds=bounds,
+        constraints=constraints,
+        options={"tol": 1e-10, "maxiter": 1e4},
+    )
 
 
 # The mean-variance frontier relies on in-sample, backward-looking optimization. In practice, portfolio optimization requires forward-looking input. Unfortunately, expected returns are notoriously difficult to estimate accurately.
-# 
-# The covariance matrix can be estimated somewhat more reliably, which has given rise to several alternative approaches. However, covariance matrices with correlated assets pose computational challenges since the optimization problem requires inverting the matrix. The high condition number induces numerical instability, which in turn gives rise to Markovitz curse: the more diversification is required (by correlated investment opportunities), the more unreliable the weights produced by the algorithm. 
+#
+# The covariance matrix can be estimated somewhat more reliably, which has given rise to several alternative approaches. However, covariance matrices with correlated assets pose computational challenges since the optimization problem requires inverting the matrix. The high condition number induces numerical instability, which in turn gives rise to Markovitz curse: the more diversification is required (by correlated investment opportunities), the more unreliable the weights produced by the algorithm.
 
 # ## Min Volatility Portfolio
 
@@ -462,13 +491,15 @@ def min_vol_target(mean_ret, cov, target, short=False):
 def min_vol(mean_ret, cov, short=False):
     bounds = ((-1 if short else 0, 1),) * n_assets
 
-    return minimize(fun=portfolio_std,
-                    x0=x0,
-                    args=(mean_ret, cov),
-                    method='SLSQP',
-                    bounds=bounds,
-                    constraints=weight_constraint,
-                    options={'tol': 1e-10, 'maxiter': 1e4})
+    return minimize(
+        fun=portfolio_std,
+        x0=x0,
+        args=(mean_ret, cov),
+        method="SLSQP",
+        bounds=bounds,
+        constraints=weight_constraint,
+        options={"tol": 1e-10, "maxiter": 1e4},
+    )
 
 
 # In[40]:
@@ -514,7 +545,7 @@ max_sharpe_perf = portfolio_performance(max_sharpe_pf.x, mean_returns, cov_matri
 
 
 r, sd = max_sharpe_perf
-pd.Series({'ret': r, 'sd': sd, 'sr': (r-rf_rate)/sd})
+pd.Series({"ret": r, "sd": sd, "sr": (r - rf_rate) / sd})
 
 
 # From simulated pf data
@@ -535,52 +566,54 @@ min_vol_perf = portfolio_performance(min_vol_pf.x, mean_returns, cov_matrix)
 
 ret_range = np.linspace(simul_perf.iloc[:, 1].min(), simul_perf.iloc[:, 1].max(), 50)
 eff_pf = efficient_frontier(mean_returns, cov_matrix, ret_range, short=True)
-eff_pf = pd.Series(dict(zip([p['fun'] for p in eff_pf], ret_range)))
+eff_pf = pd.Series(dict(zip([p["fun"] for p in eff_pf], ret_range)))
 
 
 # ### Plot Result
 
-# The simulation yields a subset of the feasible portfolios, and the efficient frontier identifies the optimal in-sample return-risk combinations that were achievable given historic data. 
-# 
-# The below figure shows the result including the minimum variance portfolio and the portfolio that maximizes the SR and several portfolios produce by alternative optimization strategies. The efficient frontier 
+# The simulation yields a subset of the feasible portfolios, and the efficient frontier identifies the optimal in-sample return-risk combinations that were achievable given historic data.
+#
+# The below figure shows the result including the minimum variance portfolio and the portfolio that maximizes the SR and several portfolios produce by alternative optimization strategies. The efficient frontier
 
 # In[48]:
 
 
 fig, ax = plt.subplots()
-simul_perf.plot.scatter(x=0, y=1, c=2, ax=ax, cmap='Blues',alpha=0.25, 
-                        figsize=(14, 9), colorbar=True)
+simul_perf.plot.scatter(
+    x=0, y=1, c=2, ax=ax, cmap="Blues", alpha=0.25, figsize=(14, 9), colorbar=True
+)
 
-eff_pf[eff_pf.index.min():].plot(linestyle='--', lw=2, ax=ax, c='k',
-                                 label='Efficient Frontier')
+eff_pf[eff_pf.index.min() :].plot(
+    linestyle="--", lw=2, ax=ax, c="k", label="Efficient Frontier"
+)
 
 r, sd = max_sharpe_perf
-ax.scatter(sd, r, marker='*', color='k', s=500, label='Max Sharpe Ratio PF')
+ax.scatter(sd, r, marker="*", color="k", s=500, label="Max Sharpe Ratio PF")
 
 r, sd = min_vol_perf
-ax.scatter(sd, r, marker='v', color='k', s=200, label='Min Volatility PF')
+ax.scatter(sd, r, marker="v", color="k", s=200, label="Min Volatility PF")
 
 kelly_wt = precision_matrix.dot(mean_returns).clip(lower=0).values
 kelly_wt /= np.sum(np.abs(kelly_wt))
 r, sd = portfolio_performance(kelly_wt, mean_returns, cov_matrix)
-ax.scatter(sd, r, marker='D', color='k', s=150, label='Kelly PF')
+ax.scatter(sd, r, marker="D", color="k", s=150, label="Kelly PF")
 
 std = weekly_returns.std()
 std /= std.sum()
 r, sd = portfolio_performance(std, mean_returns, cov_matrix)
-ax.scatter(sd, r, marker='X', color='k', s=250, label='Risk Parity PF')
+ax.scatter(sd, r, marker="X", color="k", s=250, label="Risk Parity PF")
 
-r, sd = portfolio_performance(np.full(n_assets, 1/n_assets), mean_returns, cov_matrix)
-ax.scatter(sd, r, marker='o', color='k', s=200, label='1/n PF')
+r, sd = portfolio_performance(np.full(n_assets, 1 / n_assets), mean_returns, cov_matrix)
+ax.scatter(sd, r, marker="o", color="k", s=200, label="1/n PF")
 
 
 ax.legend(labelspacing=0.8)
-ax.set_xlim(0, eff_pf.max()+.4)
-ax.set_title('Mean-Variance Efficient Frontier', fontsize=16)
-ax.yaxis.set_major_formatter(FuncFormatter(lambda y, _: '{:.0%}'.format(y)))
-ax.xaxis.set_major_formatter(FuncFormatter(lambda y, _: '{:.0%}'.format(y)))
+ax.set_xlim(0, eff_pf.max() + 0.4)
+ax.set_title("Mean-Variance Efficient Frontier", fontsize=16)
+ax.yaxis.set_major_formatter(FuncFormatter(lambda y, _: "{:.0%}".format(y)))
+ax.xaxis.set_major_formatter(FuncFormatter(lambda y, _: "{:.0%}".format(y)))
 sns.despine()
-fig.tight_layout();
+fig.tight_layout()
 
 
 # # Clean-up
@@ -589,7 +622,7 @@ fig.tight_layout();
 # In[49]:
 
 
-#1.54Gb
+# 1.54Gb
 os.remove("./assets.h5")
 
 
@@ -598,16 +631,12 @@ os.remove("./assets.h5")
 
 # <div class="alert alert-warning">
 # <font color=black>
-# 
+#
 # - [How to download the data](https://github.com/stefan-jansen/machine-learning-for-trading/blob/main/data/create_datasets.ipynb)
 # - [This notebook original code](https://github.com/stefan-jansen/machine-learning-for-trading/blob/main/05_strategy_evaluation/04_mean_variance_optimization.ipynb)
 # - Jansen, Stefan. Hands-On Machine Learning for Algorithmic Trading: Design and implement investment strategies based on smart algorithms that learn from data using Python. Packt Publishing Ltd, 2018.
-# 
+#
 # </font>
 # </div>
 
 # In[ ]:
-
-
-
-
--- tutorials/testing/From_notebook_to_python/Backtesting a Forecasting Strategy.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# https://www.quantstart.com/articles/Backtesting-a-Forecasting-Strategy-for-the-SP500-in-Python-with-pandas/
-
-# In[ ]:
